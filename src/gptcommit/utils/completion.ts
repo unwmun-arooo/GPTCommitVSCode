08f4ee02a3e4ab6fbcb54fbbdbd98b71989a4462
unwmun <unwmun@arooo.co.kr>
2023-06-08 18:33:23 +0900

    feat(completion.ts): 한국어로 commit 메시지 작성

--- src/gptcommit/utils/completion.ts
+++ src/gptcommit/utils/completion.ts
@@ -4,16 +4,19 @@
  * The original code can be found at https://github.com/di-sukharev/opencommit/blob/master/src/generateCommitMessageFromGitDiff.ts.
  */
 
-import { ChatCompletionRequestMessage, ChatCompletionRequestMessageRoleEnum } from "openai";
+import {
+  ChatCompletionRequestMessage,
+  ChatCompletionRequestMessageRoleEnum,
+} from "openai";
 
 const initMessagesPrompt: Array<ChatCompletionRequestMessage> = [
-    {
-        role: ChatCompletionRequestMessageRoleEnum.System,
-        content: `You are to act as the author of a commit message in git. Your mission is to create clean and comprehensive commit messages in the conventional commit convention. I'll send you an output of 'git diff --staged' command, and you convert it into a commit message. Do not preface the commit with anything, use the present tense. Don't add any descriptions to the commit, only commit message. Use english language to answer.`
-    },
-    {
-        role: ChatCompletionRequestMessageRoleEnum.User,
-        content: `diff --git a/src/server.ts b/src/server.ts
+  {
+    role: ChatCompletionRequestMessageRoleEnum.System,
+    content: `You are to act as the author of a commit message in git. Your mission is to create clean and comprehensive commit messages in the conventional commit convention. I'll send you an output of 'git diff --staged' command, and you convert it into a commit message. Do not preface the commit with anything, use the present tense. Don't add any descriptions to the commit, only commit message. Use korean language to answer.`,
+  },
+  {
+    role: ChatCompletionRequestMessageRoleEnum.User,
+    content: `diff --git a/src/server.ts b/src/server.ts
     index ad4db42..f3b18a9 100644
     --- a/src/server.ts
     +++ b/src/server.ts
@@ -34,24 +37,24 @@ const initMessagesPrompt: Array<ChatCompletionRequestMessage> = [
     -  console.log(\`Server listening on port \${port}\`);
     +app.listen(process.env.PORT || PORT, () => {
     +  console.log(\`Server listening on port \${PORT}\`);
-      });`
-    },
-    {
-        role: ChatCompletionRequestMessageRoleEnum.Assistant,
-        content: `fix(server.ts): change port variable case from lowercase port to uppercase PORT
-        feat(server.ts): add support for process.env.PORT environment variable`
-    }
+      });`,
+  },
+  {
+    role: ChatCompletionRequestMessageRoleEnum.Assistant,
+    content: `fix(server.ts): 포트 변수 대소문자를 소문자 포트에서 대문자 PORT로 변경
+        feat(server.ts): process.env.PORT 환경 변수에 대한 지원 추가`,
+  },
 ];
 
 export const generateCommitMessageChatCompletionPrompt = (
-    diff: string
+  diff: string
 ): Array<ChatCompletionRequestMessage> => {
-    const chatContextAsCompletionRequest = [...initMessagesPrompt];
+  const chatContextAsCompletionRequest = [...initMessagesPrompt];
 
-    chatContextAsCompletionRequest.push({
-        role: ChatCompletionRequestMessageRoleEnum.User,
-        content: diff
-    });
+  chatContextAsCompletionRequest.push({
+    role: ChatCompletionRequestMessageRoleEnum.User,
+    content: diff,
+  });
 
-    return chatContextAsCompletionRequest;
+  return chatContextAsCompletionRequest;
 };
